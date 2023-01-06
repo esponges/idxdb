b820db5bad8a23bc7e1d8b30084cc5950868a38e
esponges <esponges@gmail.com>
2023-01-05 20:38:23 -0600

    add users to store

--- lib/db.ts
+++ lib/db.ts
@@ -8,7 +8,11 @@ export interface User {
   email: string;
 }
 
-export const initDB = (): Promise<boolean> => {
+export const Stores = {
+  Users: 'users',
+};
+
+export const initDB = (): Promise<boolean|IDBDatabase> => {
   return new Promise((resolve) => {
     request = indexedDB.open('myDB');
 
@@ -16,22 +20,46 @@ export const initDB = (): Promise<boolean> => {
     request.onupgradeneeded = () => {
       db = request.result;
 
-      if (!db.objectStoreNames.contains('users')) {
+      if (!db.objectStoreNames.contains(Stores.Users)) {
         console.log('Creating users store');
-        db.createObjectStore('users', { keyPath: 'id' });
+        db.createObjectStore(Stores.Users, { keyPath: 'id' });
       }
       // no need to resolve here
     };
 
-    request.onsuccess = () => {
+    request.onsuccess = (e) => {
       db = request.result;
+      // get current version and store it
       version = db.version;
-      console.log('request.onsuccess - initDB', version);
-      resolve(true);
+      resolve(request.result);
+    };
+
+    request.onerror = (e) => {
+      resolve(false);
+    };
+  });
+};
+
+export const addData = <T>(storeName: string, data: T): Promise<T|string|null> => {
+  return new Promise((resolve) => {
+    request = indexedDB.open('myDB', version);
+
+    request.onsuccess = () => {
+      console.log('request.onsuccess - addData', data);
+      db = request.result;
+      const tx = db.transaction(storeName, 'readwrite');
+      const store = tx.objectStore(storeName);
+      store.add(data);
+      resolve(data);
     };
 
     request.onerror = () => {
-      resolve(false);
+      const error = request.error?.message
+      if (error) {
+        resolve(error);
+      } else {
+        resolve('Unknown error');
+      }
     };
   });
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
 import { useState } from 'react';
-import { initDB } from '../lib/db';
+import { Stores, addData, initDB } from '../lib/db';
 
 export default function Home() {
   const [isDBReady, setIsDBReady] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleInitDB = async () => {
     const status = await initDB();
-    setIsDBReady(status);
+    setIsDBReady(!!status);
   };
+
+  const handleAddUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+      email: { value: string };
+    };
+
+    const name = target.name.value;
+    const email = target.email.value;
+    const id = Date.now();
+
+    if (name.trim() === '' || email.trim() === '') {
+      alert('Please enter a valid name and email');
+      return;
+    }
+
+    try {
+      const res = await addData(Stores.Users, { name, email, id });
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
+  };
+
 
   return (
     <main style={{ textAlign: 'center', marginTop: '3rem' }}>
@@ -15,7 +45,16 @@ export default function Home() {
       {!isDBReady ? (
         <button onClick={handleInitDB}>Init DB</button>
       ) : (
-        <h2>DB is ready</h2>
+        <>
+          <h2>DB is ready</h2>
+          {/* add user form */}
+          <form onSubmit={handleAddUser}>
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <button type="submit">Add User</button>
+          </form>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </>
       )}
     </main>
   );
